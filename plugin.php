316080ed026bff611a8845c58b6521efb1f0f82e
Aditya Ghosh <adghosh@atlassian.com>
2020-09-11 12:37:47 +0200

    Added editor

--- plugin.php
+++ plugin.php
@@ -1,13 +1,9 @@
 <?php
 /**
- * Plugin Name: newcontent — CGB Gutenberg Block Plugin
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: newcontent — is a Gutenberg plugin created via create-guten-block.
- * Author: mrahmadawais, maedahbatool
- * Author URI: https://AhmadAwais.com/
- * Version: 1.0.0
- * License: GPL2+
- * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ * Plugin Name: NewsLetter - CTA
+ * Plugin URI: https://github.com/adityacl12345/Sunacare.git
+ * Description: NewsletterCTA — is a Gutenberg plugin created via create-guten-block.
+ * Author: Aditya, Ghosh
  *
  * @package CGB
  */
--- src/block/block.js
+++ src/block/block.js
@@ -9,10 +9,7 @@
 import './style.scss';
 import './editor.scss';
 import {
-    RichText,
-    MediaUpload,
-    BlockControls,
-    AlignmentToolbar
+    RichText
 } from "@wordpress/block-editor";
 
 // PART 2: Setup references to external functions
@@ -33,51 +30,73 @@ registerBlockType( 'cgb/block-newcontent', {
   ],
   attributes: {
     title: {
-        type: "array",
-        source: "children",
-        selector: ".callout-title"
+      type: "array",
+      source: "children",
+      selector: ".callout-title"
+    },
+    button: {
+      type: "array",
+      source: "children",
+      selector: ".callout-button"
     }
   },
   
   // PART 3.2: Markup in editor
   edit: function( props ) {
-	const {
-        attributes: { title },
+	  const {
+        attributes: { title, button },
         setAttributes
     } = props;
 
     const onChangeTitle = value => {
-		setAttributes({ title: value });
-	};
+      setAttributes({ title: value });
+    };
+    const onChangeButton = value => {
+      setAttributes({ button: value });
+    };
 	
     return (
-		<div className={props.className}>
-			<p>Sign up for newsletters.</p>
-			<RichText
-				tagName="h2"
-				className="callout-title"
-				placeholder={__("Write a callout title…")}
-				value={title}
-				onChange={onChangeTitle}
-			/>
-		</div>
-	);
+      <div className={props.className}>
+        <RichText
+          tagName="p"
+          className="callout-title"
+          placeholder={__("Sign up text")}
+          value={title}
+          onChange={onChangeTitle}
+        />
+        <a className="CTABtn">
+          <RichText
+            tagName="span"
+            className="callout-button"
+            placeholder={__("Button text")}
+            value={button}
+            onChange={onChangeButton}
+          />
+        </a>
+      </div>
+    );
   },
   
   // PART 3.3: Markup saved to database
   save: function( props ) {
-	const {
-        attributes: { title }
+	  const {
+        attributes: { title, button }
     } = props;
     return (
-		<div className={props.className}>
-			<p>Sign up for newsletters.</p>
-			<RichText.Content
-				tagName="h2"
-				className="callout-title"
-				value={title}
-			/>
-		</div>
+      <div className={props.className}>
+        <RichText.Content
+          tagName="p"
+          className="callout-title"
+          value={title}
+        />
+        <a className="CTABtn">
+          <RichText.Content
+            tagName="span"
+            className="callout-button"
+            value={button}
+          />
+        </a>
+      </div>
     );
   },
-} );
+});
